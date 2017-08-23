5f1cafea9704b73b57df7973c9bb0f5baa3f0b13
david <justforjobonly@126.com>
2017-08-23 09:39:57 +0800

    init_project

---
+++ app/src/main/java/com/dave/wifirealtimespeeker/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dave.wifirealtimespeeker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+2243543
